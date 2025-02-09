e055cd2fd2fdbbc6eeaad258b75ba19643baed2d
hax0-r <talha18513@gmail.com>
2025-02-09 14:05:36 +0500

    Entire website converted

--- src/app/culture/page.js
+++ src/app/culture/page.js
@@ -1,5 +1,5 @@
 "use client"
-import React, { useState } from 'react'
+import { useState } from 'react'
 import Navbar from '../Components/Navbar'
 import { POLL_DATA } from '@/Data/PollVotes'
 import Votes from '../Components/Votes'
